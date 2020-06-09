23c0676033602ceee63754bf378725677de7518e
Justin <jheisler01@gmail.com>
2020-06-08 20:27:40 -0600

    finished basxe layout for page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -18,7 +18,7 @@ const Landing: React.FC = () => {
         toggleNav={handleShowNav} 
       />
       <NamaslayPanel
-        navState={showNav} 
+        panelSize={showNav ? 'small' : 'large'} 
       />
       <div className="Landing__tr"></div>
       <div className="Landing__bl"></div>
--- src/pages/Schedule/Schedule.tsx
+++ src/pages/Schedule/Schedule.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
 
-import NamaslayPanel from '../../components/panels/NamaslayPanel/NamaslayPanel'
+// ?? Components
 import Nav from '../../components/Nav/Nav';
+
+// ?? Box Components
+
+// ?? Panel Components
+import NamaslayPanel from '../../components/panels/NamaslayPanel/NamaslayPanel'
+
 
 import './Schedule.scss';
 
@@ -17,9 +23,55 @@ const Schedule: React.FC = () => {
         navState={showNav}
         toggleNav={handleShowNav}  
       />
-      <NamaslayPanel
-        navState={showNav} 
-      />
+      <div className="Schedule__namaslay">
+        <NamaslayPanel
+          panelSize='small' 
+        />
+      </div>
+    
+      <div className="Schedule__classesLeft">
+        Classes Left
+      </div>
+
+      <div className="Schedule__calendar">
+        Calendar
+      </div>
+
+      <div className="Schedule__classSelection">
+        Class Selection
+      </div>
+    
+      <div className="Schedule__dynamicSelection">
+        Dynamic Selection
+      </div>
+
+      <div className="Schedule__teacherFilter">
+        Teacher Filter
+      </div>
+
+      <div className="Schedule__disciplineFilter">
+        Discipline Filter
+      </div>
+
+      <div className="Schedule__eventFilter">
+        Event Filter
+      </div>
+
+      <div className="Schedule__difficultyFilter">
+        Difficulty Filter
+      </div>
+
+      <div className="Schedule__clearFilters">
+        Clear Filters
+      </div>
+
+      <div className="Schedule__teacherInfo">
+        Teacher Info
+      </div>
+
+      <div className="Schedule__classInfo">
+        class info
+      </div>
     </div>
   )
 }
