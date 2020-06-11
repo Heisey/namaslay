d658f22ec4aa042d752ebcaebdd522c49e1fddb1
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-06-10 19:33:30 -0700

    classes on pageload

--- src/components/panels/DynamicDataPanel/DynamicDataPanel.tsx
+++ src/components/panels/DynamicDataPanel/DynamicDataPanel.tsx
@@ -26,7 +26,7 @@ const DynamicDataPanel: React.FC<DynamicDataPanelProps> = props => {
   return (
     <div className="DynamicDataPanel">
       {data && <div className="DynamicDataPanel__grid">{details}</div>}
-      {data.length === 0 && <h1>Puppies</h1>}
+      {data.length === 0 && <h1></h1>}
     </div>
   )
 }
