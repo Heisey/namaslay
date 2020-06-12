ed43449d401ab3b05f8fac970c4e2830024dc410
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-06-12 15:32:52 -0700

    user login integration

--- src/components/panels/LoginPanel/LoginPanel.js
+++ src/components/panels/LoginPanel/LoginPanel.js
@@ -1,40 +1,76 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
+import qs from 'qs'
+import './LoginPanel.scss'
+import { log } from 'util'
 import SnakeBorderButton from '../../Buttons/SnakeBorderButton/SnakeBorderButton'
 
-import './LoginPanel.scss'
+const config = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+
 
 export default function LoginPanel(props) {
 
   const { showPanel } = props
 
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const onChangeEmail = e => {
+    e.preventDefault();
+    setEmail(e.target.value)
+  }
+  const onChangePassword = e => {
+    e.preventDefault();
+    setPassword(e.target.value)
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    const requestBody = { email, password }
+    console.log(requestBody);
+
+
+    axios.post(`/students/login`, qs.stringify(requestBody), config)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+
+  }
+
   return (
     <div className={`LoginPanel LoginPanel--${showPanel ? 'show' : 'hidden'}`}>
       <form
+        onSubmit={handleSubmit}
         className="LoginPanel__form"
-        method="POST"
-        action="/"
       >
         <h1 className="LoginPanel__form--title">LOGIN</h1>
         <input
           className="LoginPanel__form--email"
           type="text"
-          name=""
+          name="email"
           placeholder="EMAIL"
+          onChange={onChangeEmail}
         />
 
         <input
           className="LoginPanel__form--password"
           type="password"
-          name=""
+          name="password"
           placeholder="PASSWORD"
+          onChange={onChangePassword}
         />
 
         <button
           className="LoginPanel__form--button"
           type="submit"
           name=""
-          // placeholder="EMAIL"
+        // placeholder="EMAIL"
         >
           SUBMIT
         </button>
