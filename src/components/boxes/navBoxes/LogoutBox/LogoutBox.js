9272185a86f240ce39cf0878da00bcfaa0f8f18e
Heisey <jheisler01@gmail.com>
2020-06-15 14:14:32 -0700

    finished nav column animations

--- src/components/boxes/navBoxes/LogoutBox/LogoutBox.js
+++ src/components/boxes/navBoxes/LogoutBox/LogoutBox.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './LogoutBox.scss';
 
+import SwordLogo from '../../../logos/SwordLogo/SwordLogo'
+
 const LogoutBox = props => {
 
   const { navState } = props
 
   return (
     <div className={`LogoutBox LogoutBox--${!navState ? 'small' : 'large'}`}>
-      Logout
+      <span className="LogoutBox__title--one">LOG</span>
+      <span className="LogoutBox__title--two">OUT</span>
+      <div className="LogoutBox__sword">
+        <SwordLogo />
+      </div>
     </div>
   )
 }
--- src/components/boxes/navBoxes/StoriesBox/StoriesBox.js
+++ src/components/boxes/navBoxes/StoriesBox/StoriesBox.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './StoriesBox.scss';
 
+import SwordLogo from '../../../logos/SwordLogo/SwordLogo'
+
 const StoriesBox = props => {
 
   const { navState } = props
 
   return (
     <div className={`StoriesBox StoriesBox--${!navState ? 'small' : 'large'}`}>
-      Stories Box
+      <span className="StoriesBox__title--one">STOR</span>
+      <span className="StoriesBox__title--two">IES</span>
+      <div className="StoriesBox__sword">
+        <SwordLogo />
+      </div>
     </div>
   )
 }
