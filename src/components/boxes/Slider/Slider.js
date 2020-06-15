63c92478ae399284637340b7c4581b118720e4fa
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-06-14 21:50:31 -0700

    slider and stuff

---
+++ src/components/boxes/Slider/Slider.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import './Slider.scss'
+import Slider from 'react-input-slider'
+
+export default function BreatheSlider(props) {
+
+  const { onGary } = props
+  const [sliderValue, setSliderValue] = useState({ x: 4 })
+
+  const setValueText = (x) => {
+    onGary(x)
+    if (sliderValue.x === 1) {
+      return `${sliderValue.x} second`
+    } else return `${sliderValue.x} seconds`
+  }
+
+  return (
+    <div className='Slider'>
+      <div className='Slider__container'>
+        Choose Your Breath Rate
+        <Slider xmax='12'
+          className="Slider__container-slider"
+          axis="x" x={sliderValue.x}
+          onChange={({ x }) => setSliderValue({ x })}
+        />
+        <div className='Slider__container-value'>
+          {setValueText(sliderValue.x)}</div>
+      </div>
+    </div>
+  )
+}
+
