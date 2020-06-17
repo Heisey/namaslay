8cd79eab50bd644317163fb4f40c344114ca4790
Heisey <jheisler01@gmail.com>
2020-06-16 20:43:28 -0700

    finished most of payment panel

--- src/components/panels/PaymentPanel/PaymentPanel.js
+++ src/components/panels/PaymentPanel/PaymentPanel.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import { CardElement,   useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement, PaymentRequestButtonElement } from '@stripe/react-stripe-js';
+import React, {useState} from 'react';
+import { useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement, PaymentRequestButtonElement } from '@stripe/react-stripe-js';
 import axios from 'axios'
 import './PayemntPanel.scss'
 import qs from 'qs'
+import SnakeBorderButton from '../../Buttons/SnakeBorderButton/SnakeBorderButton'
+import PersonLogo from '../../logos/PersonLogo/PersonLoop'
+import LoadingAnimation from '../../LoadingAnimation/LoadingAnimation'
 const config = {
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
 }
 
-
+const ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      fontSize: '18px',
+      color: '#ef6101',
+      letterSpacing: '0.025em',
+      '::placeholder': {
+        color: '#fdac07',
+      },
+    },
+    invalid: {
+      color: '#b50e0e',
+    },
+  },
+};
 
 export default function PaymentPanel(props) {
 
     const {passCountHandler, userId, selectedClass } = props
+
+    const [showLoading, showLoadingHandler] = useState(false)
+    const [showForm, showFormHandler] = useState(true)
+    const [showError, showErrorHandler] = useState(false)
     
   const stripe = useStripe();
   const elements = useElements();
 
 const handleSubmit = async (event) => {
-    
-    event.preventDefault();
+  showLoadingHandler(true)
+    // event.preventDefault();
 
     if (!stripe || !elements) {
         // Stripe.js has not loaded yet. Make sure to disable
@@ -31,19 +52,22 @@ const handleSubmit = async (event) => {
     // Get a reference to a mounted CardElement. Elements knows how
     // to find your CardElement because there can only ever be one of
     // each type of element.
-    const cardElement = elements.getElement(CardElement);
+    const cardElement = elements.getElement(CardNumberElement);
 
     // Use your card Element with other Stripe.js APIs
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
-      card: cardElement,
+      card: cardElement
     });
-
     if (error) {
+      showFormHandler(false)
+      showLoadingHandler(false)
+      showErrorHandler(true)
       console.log('[error]', error);
     } else {
       passCountHandler(1)
-      console.log(paymentMethod);
+      console.log('hiding form')
+      showFormHandler(false)
       // ?? need to make request to db to add a pass to the user
       //  hte user id is userId props
     }
@@ -51,10 +75,13 @@ const handleSubmit = async (event) => {
       const requestBody = { type: 'single', selectedClass }
       await axios.post(`/students/${userId}/passes`, qs.stringify(requestBody), config)
         .then((res) => {
+          showLoadingHandler(false)
           console.log(res.data);
         })
         .catch((e) => {
-          console.log(e);
+          showLoadingHandler(false)
+          showErrorHandler(true)
+          showFormHandler(false)
         })
     }
 
@@ -64,14 +91,71 @@ const handleSubmit = async (event) => {
 
   return (
     <div className="PaymentPanel">
-      <form onSubmit={handleSubmit}
-      >
-        <CardNumberElement />
-         <CardExpiryElement />
-        <CardCvcElement />
-        <button type="submit" disabled={!stripe} >Pay</button>
-      </form>
 
+      {showForm && (
+        <form 
+        onSubmit={handleSubmit}
+        className="PaymentPanel__form"
+      >
+            <div className="PaymentPanel__form--numberTitle">
+              Enter Card Number:
+            </div>
+            <div className="PaymentPanel__form--number">
+              <CardNumberElement options={ELEMENT_OPTIONS} />
+            </div>
+
+            <div className="PaymentPanel__form--expiryTitle">
+              Enter Expiry Date:
+            </div>
+            <div className="PaymentPanel__form--expiry">
+              <CardExpiryElement options={ELEMENT_OPTIONS} />
+            </div>
+
+            <div className="PaymentPanel__form--cvcTitle">
+              Enter CVC Number:
+            </div>
+            <div className="PaymentPanel__form--cvc">
+              <CardCvcElement options={ELEMENT_OPTIONS} />  
+            </div>
+            
+            <div className='PaymentPanel__form--button'>
+              <SnakeBorderButton
+                text='PURCHASE'
+                clickHandler={handleSubmit}
+              />
+            </div>
+        
+      </form>
+      )}
+      
+
+      {showLoading && (
+        <div className='PaymentPanel__loading'>
+          <LoadingAnimation />
+        </div>
+      )}
+
+      {(!showLoading && !showForm && !showError) && (
+        <div className='PaymentPanel__success'>
+          <span>SUCCESS</span>
+        </div>
+      )}
+
+      {(!showLoading && !showForm && showError) && (
+        <div className='PaymentPanel__success'>
+          <span>FAILED</span>
+        </div>
+      )}
+
+      <div className="PaymentPanel__sword--one">
+          <PersonLogo
+          />
+        </div>
+
+        <div className="PaymentPanel__sword--two">
+          <PersonLogo
+          />
+        </div>
     </div>
   )
 }
